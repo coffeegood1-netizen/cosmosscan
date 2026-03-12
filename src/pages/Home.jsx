4dd2c57d44a5db631e3d5f8537fc495180813173
info <info@enigmarv.com>
2026-03-12 23:54:40 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -147,7 +147,7 @@ export default function Home() {
         {filteredAndSorted.length > 0 && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {filteredAndSorted.map((article, idx) => (
-              <ArticleCard key={article.id || idx} article={article} index={idx} />
+              <ArticleCard key={article.id || idx} article={article} index={idx} onClick={() => setSelectedArticle(article)} />
             ))}
           </div>
         )}
